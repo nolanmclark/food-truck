c8aa379656ba5b33f7bc100b1445aa9be76a026a
Nolan Clark <nolanmclark@gmail.com>
2018-04-18 21:33:36 -0500

    Menu editing, reverting map functions

--- src/app/pages/menu-edit/menu-edit.component.ts
+++ src/app/pages/menu-edit/menu-edit.component.ts
@@ -11,16 +11,50 @@ export class MenuEditComponent implements OnInit {
 
   truckData: any;
   apiRoot: string = 'https://vs-genius.ddns.net/api/foodtruck';
+  menu: any;
+  categories: any;
+  items: any[];
 
   constructor(public http: Http, public authService: AuthService) { }
 
   ngOnInit() {
+    function checkDuplicateInObject(propertyName, inputArray) {
+      var seenDuplicate = false,
+          testObject = {};
+    
+      inputArray.map(function(item) {
+        var itemPropertyName = item[propertyName];    
+        if (itemPropertyName in testObject) {
+          testObject[itemPropertyName].duplicate = true;
+          item.duplicate = true;
+          seenDuplicate = true;
+        }
+        else {
+          testObject[itemPropertyName] = item;
+          delete item.duplicate;
+        }
+      });
+      return seenDuplicate;
+    }
+
     if(this.authService.isLoggedIn()) {
         let id = localStorage.getItem("truck_id");
         this.http.get(`${this.apiRoot}/location/${id}`).subscribe(res => {
           let response = res.json();
           this.truckData = response;
+          this.http.get(`${this.apiRoot}/menus/${id}`).subscribe(res => {
+            let response = res.json();
+            this.menu = Object.keys(response).map(i => response[i]);
+            let cat = [];
+            for(let i = 0; i < this.menu.length; i++) {
+              cat.push(this.menu[i][0].category);
+            }
+            this.categories = cat;
+            this.items = this.menu;
+            console.log(this.categories);
+            console.log(this.menu[0]);
+          });
         });
-    }
+      }
   }
 }
